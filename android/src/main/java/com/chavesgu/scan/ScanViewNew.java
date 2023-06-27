92f640bc767ab943676f6e95dbd94bbc0ef17d29
Thiago Leal Menezes <thiago.pixelado@gmail.com>
2023-06-27 10:42:22 -0300

    Removed orientation lock when whole scan area is used on Android.

--- android/src/main/java/com/chavesgu/scan/ScanViewNew.java
+++ android/src/main/java/com/chavesgu/scan/ScanViewNew.java
@@ -70,10 +70,12 @@ public class ScanViewNew extends BarcodeView implements PluginRegistry.RequestPe
 
         this.context = context;
         this.activity = activity;
-        activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         this.activityPluginBinding = activityPluginBinding;
         activityPluginBinding.addRequestPermissionsResultListener(this);
         this.scale = (double) args.get("scale");
+        if (this.scale != 1.0) {
+            activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
 
         checkPermission();
     }
